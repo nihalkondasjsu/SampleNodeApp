35914a669b0ff925566a8f5f8e4e6a16dcfc6321
Nihal Konda <nihal.konda@sjsu.edu>
2020-04-01 10:06:30 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,3 @@
+const ikmvapilib = require('ikmv-sampleisomorphicapilibrary')
+
+ikmvapilib.sendMail('konda.nihal5@gmail.com','Node: Isomorphic Test Report','<h1>Passed '+new Date().getTime()+'</h1>')
